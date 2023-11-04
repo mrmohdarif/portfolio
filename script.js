70997aa10febcd78e5aa4d9ffb46176e5050dc39
mohd arif <alliswell5solution@gmail.com>
2023-11-05 02:16:27 +0530

    0/2

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed=new Typed('.typing,.about_typing',{
+    strings:["Fullstack Developper","Frontend Developper","Backend Developper"],
+     typeSpeed:100,
+     BackSpeed:60,
+     loop:true
+})
+
